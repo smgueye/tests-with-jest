ff6b44d088bcce4deedf323c32e728f821927c77
smgueye <gueye.syd.mouhamed@gmail.com>
2022-03-04 15:17:46 +0000

    (test): Model - Testing instantiation. - Testing structure.

---
+++ model.js
@@ -0,0 +1,14 @@
+export default class Model {
+
+  constructor() {
+    this.$collection = [];
+  }
+
+  all() { }
+
+  record() { }
+
+  find() { }
+
+  update() { }
+}
--- user.spec.js
+++ user.spec.js
@@ -5,4 +5,11 @@ describe('User', () => {
     const user = new User({ name: "Seydina Mouhamed Gueye", email: "smg@smgueye.io" });
     expect(user.info).toBe("Seydina Mouhamed Gueye")
   })
+
+  /*const result = {
+    value: new Date()
+  }
+  expect(result).toEqual({
+    value: expect.any(Number)
+  })*/
 })
